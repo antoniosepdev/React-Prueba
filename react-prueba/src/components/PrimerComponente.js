39679d905836c0ed57e89567e2d6bb4cd7b390bf
Antonio Sepúlveda <antonio.sepulveda.calderon1@gmail.com>
2022-11-07 13:53:28 -0300

    Primera prueba con React
    Se suben todos los archivos.

---
+++ react-prueba/src/components/PrimerComponente.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const PrimerComponente = () => {
+  return (
+    <div>
+        <h1>Primera prueba con React</h1>
+        <h3>Prueba realizada con GitHub</h3>
+    </div>
+  )
+}
---
+++ react-prueba/src/components/SegundoComponente.js
@@ -0,0 +1,30 @@
+import React,{useState} from 'react';
+
+
+export const SegundoComponente = () => {
+
+//    let nombre = "Antonio";
+    let web = "antoniosepdev.github.io";
+    let lenguajes =["C#","Java","Python","JavaScript"];
+    const [nombre, setnombre] = useState("Antonio");
+    const cambiarnombre = (nuevonombre) =>{
+        setnombre(nuevonombre);
+    }
+
+
+  return (
+    <div>
+        <p>
+            Mi nombre es: {nombre}
+        </p>
+        <p> Mi web es: {web}</p>
+        <h3>Lenguajes de programación:</h3>
+        <button onClick={e=>cambiarnombre("Antonio SepDev")}>Cambiar nombre</button>        
+        <ul>
+            {lenguajes.map((lenguaje,indice)=>{
+                return(<li key={indice}>{lenguaje}</li>)
+            })}
+        </ul>
+    </div>
+  )
+}
